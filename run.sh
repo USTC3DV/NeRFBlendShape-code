a9b5d388062377ff260622030e391d454edb7f24
XuanGhahahaha <xghahahaha@gmail.com>
2022-12-28 01:17:35 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 NAME=id1
 
 CUDA_VISIBLE_DEVICES=0 python run_nerfblendshape.py\
-    --img_idpath ./dataset/$NAME/transforms_train.json \
-    --exp_idpath ./dataset/$NAME/transforms_train.json \
-    --pose_idpath ./dataset/$NAME/transforms_train.json \
-    --intr_idpath ./dataset/$NAME/transforms_train.json \
+    --img_idpath ./dataset/$NAME/transforms.json \
+    --exp_idpath ./dataset/$NAME/transforms.json \
+    --pose_idpath ./dataset/$NAME/transforms.json \
+    --intr_idpath ./dataset/$NAME/transforms.json \
     --use_checkpoint ./dataset/$NAME/pretrained.pth.tar \
     --workspace trial_$NAME \
     --fp16 --tcnn  --cuda_ray --basis_num 46   --add_mean  --use_lpips  --mode normal_test
